614a4bfc538c41226e0dedcca4f994696a6b46b6
lmorenof2021 <l.morenof.2021@alumnos.urjc.es>
2025-02-24 12:06:34 +0100

    arreglo ruta y lectura archivo, errores

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,74 +1,92 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const url = require('url');
+
 const PORT = 8001;
+
+//leer archivo y manejar errores
+const serveFile = (res, filePath, contentType) => {
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            console.error('Error al leer el archivo:', err);
+            res.writeHead(500);
+            res.end('Error al cargar el archivo');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content);
+        }
+    });
+};
+
 
 
 // Crear el servidor
 const server = http.createServer((req, res) => {
     console.log(`Solicitud recibida para: ${req.url}`);
 
-    // Manejo de rutas
+// Manejo de rutas
     let filePath = ''; //almacena la ruta del archivo
     let contentType = ''; //almacena el tipo de contenido del archivo
 
     if (req.url === '/') {
 
-        filePath = path.join(__dirname, 'p1', 'index.html');
+        filePath = path.join(__dirname, 'index.html');
     
         contentType = 'text/html';
     
     } else if (req.url === '/producto1') {
     
-        filePath = path.join(__dirname, 'p1', 'producto1.html');
+        filePath = path.join(__dirname, 'producto1.html');
     
         contentType = 'text/html';
     
     } else if (req.url === '/producto2') {
     
-        filePath = path.join(__dirname, 'p1', 'producto2.html');
+        filePath = path.join(__dirname, 'producto2.html');
     
         contentType = 'text/html';
     
     } else if (req.url === '/producto3') {
     
-        filePath = path.join(__dirname, 'p1', 'producto3.html');
+        filePath = path.join(__dirname,  'producto3.html');
     
         contentType = 'text/html';
     
     } else if (req.url === '/css/styles.css') {
     
-        filePath = path.join(__dirname, 'p1', 'css', 'styles.css');
+        filePath = path.join(__dirname,  'css', 'styles.css');
     
         contentType = 'text/css';
     
     } else if (req.url === '/js/script.js') {
     
-        filePath = path.join(__dirname, 'p1', 'js', 'script.js');
+        filePath = path.join(__dirname,  'js', 'script.js');
     
         contentType = 'application/javascript';
     
     } else if (req.url === '/imagenes/producto1.jpg') {
     
-        filePath = path.join(__dirname, 'p1', 'imagenes', 'producto1.jpg');
+        filePath = path.join(__dirname,  'imagenes', 'producto1.jpg');
     
         contentType = 'image/jpeg';
     
     } else if (req.url === '/imagenes/producto2.jpg') {
     
-        filePath = path.join(__dirname, 'p1', 'imagenes', 'producto2.jpg');
+        filePath = path.join(__dirname,  'imagenes', 'producto2.jpg');
     
         contentType = 'image/jpeg';
     
     } else if (req.url === '/imagenes/producto3.jpg') {
     
-        filePath = path.join(__dirname, 'p1', 'imagenes', 'producto3.jpg');
+        filePath = path.join(__dirname,  'imagenes', 'producto3.jpg');
     
         contentType = 'image/jpeg';
     
     
     }
+
+
+    
     
     
     serveFile(res, filePath, contentType);
